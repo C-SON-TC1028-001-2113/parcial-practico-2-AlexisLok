20173c6edb53ed0d5156f40899677b9e30675059
AlexisLok <a01251886@itesm.mx>
2021-09-10 17:02:29 +0000

    Ejercicio 2 de examen 1

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -3,7 +3,11 @@
 def main():
     numero = int(input("Escribe un numero : "))
     #escribe tu código abajo de esta línea
-
+    ele = 1
+    #Abajo se evalua la variable al cuadrado y se compara con el valor
+    while ele**2 <= numero:
+        ele += 1 #Si la variable al cuadrado es menor a numero, entonces se suma 1 y se vuelve a probar
+    print (ele)
 
 if __name__=='__main__':
     main()
